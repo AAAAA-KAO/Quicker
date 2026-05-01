d65ab3276c7e07e96834f69dc37736b96841ad9c
Alan Thomas <1500649027@qq.com>
2026-05-01 15:10:24 +0800

    1. 调整并成功运行Phase1-question_decomposition的代码 2. 调整Phase2-literature_search的代码，但还未成功运行，需较好的网络连接

--- Phase1-question_dcomposition.py
+++ Phase1-question_dcomposition.py
@@ -1,51 +1,55 @@
-from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
-from langchain_core.prompts import PromptTemplate
-from langchain_core.exceptions import OutputParserException
-from langchain_core.output_parsers import StrOutputParser
+import json
+import os
+import hashlib
+import argparse
+import pandas as pd
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 from langchain_core.runnables import (
     RunnableLambda,
     RunnableParallel,
     RunnablePassthrough,
 )
-import json
-import os
-import hashlib
-import pandas as pd
-from pydantic import BaseModel, Field
-from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import PromptTemplate
+from langchain_core.exceptions import OutputParserException
+from langchain_core.output_parsers import StrOutputParser
 load_dotenv(".env")
 
-from utils.PICO.prompt import get_zero_shot_pipeline_prompt,get_few_shot_pipeline_prompt
-from utils.PICO.few_shot import match_few_shot, create_example_selector
-from utils.PICO.pfe import generate_experience,generate_answer,combine_examples_with_experience
 from utils.PICO.base import create_dataset
+from utils.PICO.few_shot import match_few_shot, create_example_selector
 from utils.logger import setup_loggers, get_workflow_logger, get_detail_logger
-
-
-# Hyperparameters
-YOUR_CONFIG_PATH = 'config/config.json'  # your config path
-YOUR_QUESTION_DECOMPOSITION_PATH =  'data/2021ACR RA/Question_Decomposition' # e.g. 'data/2021ACR RA/Question_Decomposition'
-
-method = 'zero-shot'
-dataset_name =  '2021ACR RA' # Choose from: 2021ACR RA, 2020EAN Dementia, 2024KDIGO CKD. If you want to use your own dataset, please modify the code accordingly.
-clinical_question = "Should patients with RA on DMARDs who are in low disease activity gradually taper off DMARDs, abruptly withdraw DMARDs, or continue DMARDS at the same doses?" # Example: "In RA patients who have achieved sustained remission for over one year with DMARD monotherapy, is drug discontinuation advisable?"
+from utils.PICO.prompt import get_zero_shot_pipeline_prompt,get_few_shot_pipeline_prompt
+from utils.PICO.pfe import generate_experience,generate_answer,combine_examples_with_experience
 
 
 # Equipped the llm with structured output
 class QuestionDecompositionOutput(BaseModel):
-    P: list[str] = Field(description="The population of the study")
-    I: list[str] = Field(description="The intervention of the study")
-    C: list[str] = Field(description="The comparison of the study")
-    O: list[str] = Field(description="The outcome of the study")
+    P: list[str] = Field(description="The population of the question")
+    I: list[str] = Field(description="The intervention of the question")
+    C: list[str] = Field(description="The comparison of the question")
+    O: list[str] = Field(description="The outcome of the question")
 
 
-def main():
-    question_index = hashlib.sha256((clinical_question).encode('utf-8')).hexdigest()[:8]
+def main(args):
+    # 解析超参数
+    YOUR_CONFIG_PATH = args.YOUR_CONFIG_PATH
+    YOUR_QUESTION_DECOMPOSITION_PATH = args.YOUR_QUESTION_DECOMPOSITION_PATH
+    method = args.method
+    dataset_name = args.dataset_name
+    clinical_question = args.clinical_question
+
+    # 配置额外参数
+    question_index = hashlib.sha256((clinical_question + dataset_name).encode('utf-8')).hexdigest()[:8]
+
+    # 配置日志
     setup_loggers(log_file=os.path.join(os.getenv("LOG_DIR"), dataset_name, "Question_Decomposition", f"{question_index}.log"))
     wf_logger = get_workflow_logger(__name__)
     dt_logger = get_detail_logger(__name__)
 
+    # --------------------任务执行---------------------
+    # 读取模型配置
     wf_logger.info(f"Start to decompose the question: {clinical_question}")
     wf_logger.info(f"【1/3】Config the model and prompt, Now you are using the {method} method")
     config_path = os.path.join(YOUR_CONFIG_PATH)
@@ -56,31 +60,37 @@ def main():
     model_name = model_config[f'question_decomposition_model']['model_name']
     api_key = model_config[f'question_decomposition_model']['API_KEY']
     api_base_URL = model_config[f'question_decomposition_model']['BASE_URL']
+
+    # 创建模型
     qd_model = ChatOpenAI(openai_api_key=api_key, base_url=api_base_URL, model=model_name)
     qd_model = qd_model.with_structured_output(QuestionDecompositionOutput)
 
-    # For zero-shot, you can use the following code:
+    # zero-shot：创建prompt
     pipeline_prompt = get_zero_shot_pipeline_prompt(dataset_name)
-    # output_parser = StrOutputParser()
+    
+    # 构建langchain链
     later_zero_shot_exp = qd_model
     local_zero_shot_chain = pipeline_prompt | RunnableParallel(
         generation_chain=later_zero_shot_exp, prompt_value=RunnablePassthrough()
     )
 
+    # 运行langchain链
     wf_logger.info(f"【2/3】Run the {method} chain")
     # Change the chain if you use the other two methods
     answer_dict = local_zero_shot_chain.invoke({"Question": clinical_question})
     dt_logger.info(f"The prompt is: {answer_dict['prompt_value']}")
-    dt_logger.info(f"The output PICO is: {answer_dict['generation_chain']}")
 
-    
-    # Extract the PICO from the answer_dict
+    # 提取PICO元素
     population = answer_dict['generation_chain'].P
     intervention = answer_dict['generation_chain'].I
     comparison = answer_dict['generation_chain'].C
     outcome = answer_dict['generation_chain'].O
+    dt_logger.info(f"Population: {population}")
+    dt_logger.info(f"Intervention: {intervention}")
+    dt_logger.info(f"Comparison: {comparison}")
+    dt_logger.info(f"Outcome: {outcome}")
 
-    # Read pico_file
+    # 读取已有的PICO
     pico_file_path = os.path.join(YOUR_QUESTION_DECOMPOSITION_PATH, 'PICO_Information.json')
     wf_logger.info(f"【3/3】Store the PICO to {pico_file_path}")
     if os.path.exists(pico_file_path):
@@ -89,7 +99,7 @@ def main():
     else:
         pico_list = []
 
-    # Add a new question to the pico_file
+    # 将本次新增的PICO添加到已有的PICO列表
     pico_dict = {}
     pico_dict['Index'] = question_index
     pico_dict['Question'] = clinical_question
@@ -102,10 +112,18 @@ def main():
     if not os.path.exists(YOUR_QUESTION_DECOMPOSITION_PATH):
         os.makedirs(YOUR_QUESTION_DECOMPOSITION_PATH)
 
-    # Save to pico information file
+    # 保存更新后的PICO列表
     with open(os.path.join(YOUR_QUESTION_DECOMPOSITION_PATH,'PICO_Information.json'), 'w', encoding="utf8") as file:
         json.dump(pico_list, file, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Decompose a question into PICO components")
+    parser.add_argument("--YOUR_CONFIG_PATH", type=str, default='config/config.json', help="The config path")
+    parser.add_argument("--YOUR_QUESTION_DECOMPOSITION_PATH", type=str, default='data/2021ACR RA/Question_Decomposition', help="The question decomposition path")
+    parser.add_argument("--method", type=str, default='zero-shot', help="The method to use")
+    parser.add_argument("--dataset_name", type=str, default='2021ACR RA', help="The dataset name")
+    parser.add_argument("--clinical_question", type=str, default="Should patients with RA on DMARDs who are in low disease activity gradually taper off DMARDs, abruptly withdraw DMARDs, or continue DMARDS at the same doses?", help="The clinical question to decompose")
+    args = parser.parse_args()
+
+    main(args)
---
+++ Phase2-literature_search.py
@@ -0,0 +1,145 @@
+import os
+import json
+import argparse
+import pandas as pd
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+load_dotenv(".env")
+
+from camel.types import TaskType, ModelType,ModelPlatformType
+from utils.Evidence_Retrieval.pubmedretrieval import PubMedRetrieval
+from utils.logger import setup_loggers, get_detail_logger, get_workflow_logger
+
+
+def main(args):
+    # 解析超参数
+    YOUR_CONFIG_PATH = args.YOUR_CONFIG_PATH
+    YOUR_QUESTION_DECOMPOSITION_PATH = args.YOUR_QUESTION_DECOMPOSITION_PATH
+    YOUR_DATASET_PATH = args.YOUR_DATASET_PATH
+    save_base = args.save_base
+    disease = args.disease
+    pico_idx = args.pico_idx
+    additional_parameters = args.additional_parameters
+    filters = args.filters
+    use_agent = args.use_agent
+    invalid_publication_types = args.invalid_publication_types
+
+    # 配置日志
+    setup_loggers(log_file=os.path.join(os.getenv("LOG_DIR"), YOUR_DATASET_PATH.split('/')[-1], "Literature_Search", f"{pico_idx}.log"))
+    wf_logger = get_workflow_logger(__name__)
+    dt_logger = get_detail_logger(__name__)
+
+    # 读取模型配置
+    config_path = os.path.join(YOUR_CONFIG_PATH)
+    with open(config_path, 'r', encoding="utf-8") as file:
+        config = json.load(file)
+    model_config = config['model']["literature_search_model"]
+    model_name = model_config['model_name']
+    base_url = model_config['BASE_URL']
+    api_key = model_config['API_KEY']
+
+    # 读取指定的pico_idx对应的PICO信息
+    question_deconstruction_datapath = os.path.join(
+    YOUR_QUESTION_DECOMPOSITION_PATH, 'PICO_Information.json'
+    )
+    question_deconstruction_data = pd.read_json(
+        question_deconstruction_datapath, dtype={'Index': str}
+    )
+    question_deconstruction_data = question_deconstruction_data[
+        question_deconstruction_data['Index'] == pico_idx
+    ]
+    original_qd_dict = question_deconstruction_data.to_dict(orient='records')  # 结果为一个Python字典
+
+    # 初始化PubMedRetrieval
+    wf_logger.info(f"Initializing PubMedRetrieval for {disease} with pico_idx {pico_idx}")
+    model_setting = {'search_term_formation': model_name,'search_strategy_formation':model_name}
+    clinical_question = original_qd_dict[0]['Question']
+    population = original_qd_dict[0]['P']
+    intervention = original_qd_dict[0]['I']
+    comparison = original_qd_dict[0]['C']
+    save_path = os.path.join(save_base, model_name, 'use_agent_' + str(use_agent))
+    pico_idx = original_qd_dict[0]['Index']
+    retriever = PubMedRetrieval(
+        disease=disease,
+        clinical_question=clinical_question,
+        population=population,
+        intervention=intervention,
+        comparison=comparison,
+        api_key=api_key,
+        base_url=base_url,
+        model_setting=model_setting,
+        use_agent=use_agent,
+        save_path=save_path,
+        pico_idx=pico_idx,
+        filters=filters, 
+        additional_parameters=additional_parameters
+    )
+
+    # 执行检索
+    # 该步骤会输出两个文件：检索策略.txt和检索结果.json
+    wf_logger.info(f"Executing PubMedRetrieval for {disease} with pico_idx {pico_idx}")
+    retriever.run()
+    dt_logger.info(retriever.search_terms)
+
+    # 加载文献检索结果
+    save_results_path = os.path.join(save_path, f'PICO{pico_idx}.json')
+    with open(save_results_path, 'r', encoding="utf8") as file:
+        search_results = json.load(file)
+
+    # Heuristic screening：移除重复和没有摘要的文献记录
+    # remove records without abstract
+    wf_logger.info(f"Remove duplicate or non-abstract or invalid publication types records")
+    dt_logger.info('Total records: '+str(len(search_results)))
+    search_results = [record for record in search_results if record['Abstract'] != None]
+    dt_logger.info('Records with abstract: '+str(len(search_results)))
+    # deduplicate
+    pmid_set = {d["Paper_Index"] for d in search_results}
+    for r in search_results:
+        if r["Paper_Index"] in pmid_set:
+            pmid_set.remove(r["Paper_Index"])
+        else:
+            search_results.remove(r)
+    dt_logger.info('Records after deduplication: '+str(len(search_results)))
+
+    # 查看文献类型
+    publication_type_set = {type for record in search_results for type in record['Publication Types']}
+    dt_logger.info(f"Publication types: {list(publication_type_set)}")
+
+    # 移除所有无效的文献类型
+    for record in search_results:
+        if any(pt in invalid_publication_types for pt in record['Publication Types']):
+            search_results.remove(record)
+    dt_logger.info('Records after removing invalid publication types: ' + str(len(search_results)))
+
+    # 保存第一、第二阶段的数据——PICO、文献检索结果
+    quicker_data = {
+        "disease": disease,
+        "clinical_question": clinical_question,
+        'pico_idx': pico_idx,
+        "population": population,
+        "intervention": intervention,
+        "comparison": comparison,
+        "search_results": search_results,
+    }
+
+    quicker_data_path = os.path.join(YOUR_DATASET_PATH, f'quicker_data(PICO_IDX{pico_idx})_ls.json')
+    with open(quicker_data_path, 'w', encoding="utf8") as file:
+        json.dump(quicker_data, file, indent=4)
+    dt_logger.info(f"Quicker data saved to {quicker_data_path}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Literature search")
+    parser.add_argument("--YOUR_CONFIG_PATH", type=str, default='config/config.json', help="The config path")
+    parser.add_argument("--YOUR_QUESTION_DECOMPOSITION_PATH", type=str, default='data/2021ACR RA/Question_Decomposition', help="The question decomposition path")
+    parser.add_argument("--YOUR_DATASET_PATH", type=str, default='data/2021ACR RA', help="The dataset path")
+    parser.add_argument("--save_base", type=str, default="data/2021ACR RA/Literature_Search/pubmed/Results", help="search results folder")
+    parser.add_argument("--disease", type=str, default='Rheumatoid Arthritis (RA)', help="The disease name")
+    parser.add_argument("--pico_idx", type=str, default='ef0e4f95', help="The PICO index of the question decomposition")
+    parser.add_argument("--additional_parameters", type=dict, default={'datetype': 'pdat', 'mindate': '1946', 'maxdate': '2025/03/30'}, help="The additional parameters to PubMedRetrieval")
+    parser.add_argument("--filters", type=dict, default={"Just search for RCT":'''<search results> AND ("Randomized controlled trial"[pt] OR "Controlled clinical trial"[pt] OR Randomized[tiab] OR Placebo[tiab] OR "Drug therapy"[sh] OR Randomly[tiab] OR Trial[tiab] OR Groups[tiab])''', 'No review': "<search results> NOT review[pt]"}, help="The search filters to use")
+    parser.add_argument("--use_agent", type=bool, default=True, help="Whether to use Agentic method or not")
+    parser.add_argument("--invalid_publication_types", type=list, default=['Comment', 'Editorial', 'Case Reports', 'News', 'Interview','Published Erratum','Observational Study','Autobiography','Address','Meta-Analysis','Retracted Publication'], help="The invalid publication types to remove")  # you can modify this list according to your needs
+    args = parser.parse_args()
+    main(args)
